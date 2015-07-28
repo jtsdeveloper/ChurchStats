7fad01a28e40db8d55b2b8467d92f3ee00d0ceba
jtsdeveloper <jtsdeveloper@gmail.com>
2015-07-28 17:50:54 -0400

    Updated JS to publish stats (no input or event)

--- js/ChurchStats.js
+++ js/ChurchStats.js
@@ -1,1 +1,17 @@
 /* Chuch stats */
+
+//create object of a Sunday's church stats
+var service = {
+    date: "July 26, 2015",
+    attendance: 35,
+    sermon: "Imagining the Unfathomable",
+}
+
+var newDate = document.getElementById("dateOutput");
+newDate.textContent = service.date;
+
+var newAttendance = document.getElementById("attendanceOutput");
+newAttendance.textContent = service.attendance;
+
+var newSermon = document.getElementById("sermonOutput");
+newSermon.textContent = service.sermon;
