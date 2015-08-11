ae490a83ff0f0d41fb028890e0955ca1ddda75a8
jtsdeveloper <jtsdeveloper@gmail.com>
2015-08-11 15:51:59 -0400

    Simple Event Listener added
    Updated

--- js/ChurchStats.js
+++ js/ChurchStats.js
@@ -1,17 +1,23 @@
 /* Chuch stats */
 
-//create object of a Sunday's church stats
-var service = {
-    date: "July 26, 2015",
-    attendance: 35,
-    sermon: "Imagining the Unfathomable",
+var elForm,newDate,newAttendance,newSermon,newDateOutput,newAttendanceOutput,newSermonOutput;
+
+elForm = document.getElementById('stats');
+
+function printData(event){
+    newDate = document.getElementById("date").value;
+    newAttendance = document.getElementById("attendance").value;
+    newSermon = document.getElementById("sermon").value;
+    newDateOutput = document.getElementById("dateOutput");
+    newAttendanceOutput = document.getElementById("attendanceOutput");
+    newSermonOutput = document.getElementById("sermonOutput");
+    
+    //write the new info into 2nd div
+    newDateOutput.innerHTML = newDate;
+    newAttendanceOutput.innerHTML = newAttendance;
+    newSermonOutput.innerHTML = newSermon;
+    event.preventDefault();
 }
 
-var newDate = document.getElementById("dateOutput");
-newDate.textContent = service.date;
+elForm.addEventListener('submit', printData, false);
 
-var newAttendance = document.getElementById("attendanceOutput");
-newAttendance.textContent = service.attendance;
-
-var newSermon = document.getElementById("sermonOutput");
-newSermon.textContent = service.sermon;
